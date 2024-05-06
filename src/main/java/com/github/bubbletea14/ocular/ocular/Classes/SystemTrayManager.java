5def2a43d7b4635248a6f7d06099ed74436cee1f
andremdiasdev <andremdias.dev@gmail.com>
2024-05-05 22:58:24 -0400

    Fixes for system tray manager and the pom.xml for building

--- src/main/java/com/github/bubbletea14/ocular/ocular/Classes/SystemTrayManager.java
+++ src/main/java/com/github/bubbletea14/ocular/ocular/Classes/SystemTrayManager.java
@@ -3,15 +3,16 @@ package com.github.bubbletea14.ocular.ocular.Classes;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.imageio.ImageIO;
 import com.github.bubbletea14.ocular.ocular.services.MetricsCollection.MetricCollector;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SystemTrayManager {
-    
-   private final MetricCollector metricCollector;
-   private static final Logger logger = LoggerFactory.getLogger(MetricCollector.class);
+
+    private final MetricCollector metricCollector;
+    private static final Logger logger = LoggerFactory.getLogger(MetricCollector.class);
 
     public SystemTrayManager(MetricCollector metricCollector) {
         this.metricCollector = metricCollector;
@@ -19,20 +20,28 @@ public class SystemTrayManager {
 
     public void createSystemTrayIcon() {
         System.setProperty("java.awt.headless", "false");
-        if (SystemTray.isSupported()) {
-            SystemTray systemTray = SystemTray.getSystemTray();
-            Image image = Toolkit.getDefaultToolkit().getImage("src\\main\\resources\\OcularIcon.jpeg");
-            PopupMenu popupMenu = new PopupMenu();
-            popupMenu.add(getExitItem());
-            TrayIcon trayIcon = new TrayIcon(image, "Ocular", popupMenu);
-            trayIcon.setImageAutoSize(true);
-            try {
-                systemTray.add(trayIcon);
-            } catch (AWTException e) {
-                logger.info("SystemTray could not be added.");
-            }
-        } else {
+        if (!SystemTray.isSupported()) {
             logger.info("SystemTray is not supported.");
+            return;
+        }
+
+        PopupMenu popupMenu = new PopupMenu();
+        popupMenu.add(getExitItem());
+
+        SystemTray systemTray = SystemTray.getSystemTray();
+        Image image = null;
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream("/OcularIcon.jpeg"));
+        } catch (Exception e) {
+            logger.error("Error loading icon image: " + e.getMessage());
+        }
+        TrayIcon trayIcon = new TrayIcon(image, "Ocular", popupMenu);
+        trayIcon.setImageAutoSize(true);
+
+        try {
+            systemTray.add(trayIcon);
+        } catch (AWTException e) {
+            logger.info("SystemTray could not be added.");
         }
         logger.info("SystemTray is added.");
     }
